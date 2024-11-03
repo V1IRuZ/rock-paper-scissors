42374c47aa449d7fef5740b18489cab97a135c0d
Esa <esa-pitkanen@hotmail.com>
2024-11-03 18:23:59 +0200

    Convert alert messages to text content
    By pressing the buttons, text appears on the page instead of alert messages. A new div element was added to the html file where the results of the game are displayed.

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    
 function getComputerChoice() {
     const randomInt = Math.floor(Math.random() * 3);
     if (randomInt === 0) {
@@ -8,64 +13,44 @@ function getComputerChoice() {
         return 'scissors';
     }
 }
-const rock = document.querySelector("#rock");
-    rock.addEventListener("click", () => {
-        alert("rock");
-    })
-const paper = document.querySelector("#paper")
-    paper.addEventListener("click", () => {
-        alert("paper");
-    })
 
-const scissors = document.querySelector("#scissors")
-    scissors.addEventListener("click", () => {
-        alert("scissors");
-    })
+const humanSelection = button.id;
+const computerSelection = getComputerChoice();
+const content = document.querySelector("div");
 
+let humanScore = 0;
+let computerScore = 0;
 
-function getHumanChoice() {
-
-}
-
-function playGame() {
-    
-    let humanScore = 0;
-    let computerScore = 0; 
-
-    
-
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-
-        function playRound(humanChoice, computerChoice) {
-            if (humanChoice === 'rock' && computerChoice === 'scissors') {
-                alert("You win! Rock beats scissors!");
-                humanScore++;
-            } else if (humanChoice === 'paper' && computerChoice === 'rock') {
-                alert("You win! Paper beats Rock!");
-                humanScore++;
-            } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
-                alert("You win! Scissors beats Paper!");
-                humanScore++;
-            } else if (humanChoice === 'rock' && computerChoice === 'paper') {
-                alert("You lose! Paper beats Rock!");
-                computerScore++;
-            } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
-                alert("You lose! Scissors beats Paper!");
-                computerScore++;
-            } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
-                alert("You lose! Rock beats Scissors!")
-                computerScore++;
-            } else {
-                alert(`It's a draw! Both chose ${humanChoice}`)
-            }
-        }
-        playRound(humanSelection, computerSelection);
-        alert( `Your score: ${humanScore}, opponent's score: ${computerScore}`);
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === 'rock' && computerChoice === 'scissors') {
+        content.textContent = "You win! Rock beats scissors!";
+        humanScore++;
+    } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+        content.textContent = "You win! Paper beats Rock!";
+        humanScore++;
+    } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+        content.textContent = "You win! Scissors beats Paper!";
+        humanScore++;
+    } else if (humanChoice === 'rock' && computerChoice === 'paper') {
+        content.textContent = "You lose! Paper beats Rock!";
+        computerScore++;
+    } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+        content.textContent = "You lose! Scissors beats Paper!";
+        computerScore++;
+    } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+        content.textContent = "You lose! Rock beats Scissors!";
+        computerScore++;
+    } else {
+        content.textContent = `It's a draw! Both chose ${humanChoice}`;
     }
+}
+playRound(humanSelection, computerSelection);
+
+});
+});  
 
    
-playGame();
+
 
 
 
